0cbe0d8783ab15da428381965d0e5564600ddd37
unknown <phamvubkvn@gmail.com>
2026-05-28 11:05:44 +0700

    Update feature XYZ

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,3 +5,9 @@ import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 
 Alpine.start();
+
+import { createIcons } from 'lucide';
+
+document.addEventListener('DOMContentLoaded', () => {
+    createIcons();
+});
